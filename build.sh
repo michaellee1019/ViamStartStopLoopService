5ddadd085cff8d778550f937a812df2bec615d61
Michael Lee <michaellee1019@gmail.com>
2024-08-09 14:52:42 -0400

    fix build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m PyInstaller --onefile --hidden-import="googleapiclient" --hidden-import="viam-wrap" src/main.py
-tar -czvf dist/archive.tar.gz dist/models
+tar -czvf dist/archive.tar.gz dist/main
